d621ebed75d3f2dfe9adbcd5ae316336c3445a10
Carlos Pizano <cpu@chromium.org>
2014-01-01 18:31:22 -0800

    adding two sample catalog files
    should be in sync with index.plex

---
+++ src/data/catalog/pxx/TestClassA.cc
@@ -0,0 +1,18 @@
+// Sample class, see index.plex.
+namespace pxx {
+class TestClassA {
+private:
+  int d_;
+
+public:
+  TestClassA(int d) : d_(d) {
+  }
+  void Method1(const char* x) {
+    if (x) throw _d;
+  }
+  int Method2(int y) {
+    return (42 * y) + d;
+  }
+};
+
+}
---
+++ src/data/catalog/pxx/TestFunA.cc
@@ -0,0 +1,7 @@
+// Sample function, see index.plex.
+namespace pxx {
+int TestFunA(int x, int y) {
+  return x + y + 42;
+}
+
+}
