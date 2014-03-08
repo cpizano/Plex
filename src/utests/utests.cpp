ab505ac22607e1575528cd62e44386b7f0492bef
Carlos Pizano <cpu@chromium.org>
2014-03-07 20:27:40 -0800

    adding unittests target
    with do-nothing cpp.

---
+++ src/utests/utests.cpp
@@ -0,0 +1,13 @@
+// utests.cpp : Defines the entry point for the console application.
+//
+
+#include <SDKDDKVer.h>
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+int wmain(int argc, wchar_t* argv[]) {
+	return 0;
+}
+
