356d88495a517e44d3228094a41767bee3c27e12
Carlos Pizano <cpu@chromium.org>
2014-05-18 11:31:13 -0700

    small changes to IOException
    In preparation for the IO classes.

--- src/utests/catalog/plx/exceptions/io.h
+++ src/utests/catalog/plx/exceptions/io.h
@@ -2,16 +2,21 @@
 ///////////////////////////////////////////////////////////////////////////////
 // plx::IOException
 // error_code_ : The win32 error code of the last operation.
+// name_ : The file or pipe in question.
 //
 namespace plx {
 class IOException : public plx::Exception {
   DWORD error_code_;
+  const std::wstring name_;
 
 public:
-  IOException(int line)
-      : Exception(line, "IO problem"), error_code_(::GetLastError()) {
+  IOException(int line, const wchar_t* name)
+      : PlexException(line, "IO problem"),
+        error_code_(::GetLastError()),
+        name_(name) {
     PostCtor();
   }
   DWORD ErrorCode() const { return error_code_; }
+  const wchar_t* Name() const { return name_.c_str(); }
 };
 }
