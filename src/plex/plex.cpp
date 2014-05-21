e39421dedeb67e03ac66e1fd5337cc85712b588b
Carlos Pizano <cpu@chromium.org>
2014-05-20 18:14:42 -0700

    increase the re-ord limit
    Now to 30

--- src/plex/plex.cpp
+++ src/plex/plex.cpp
@@ -2144,7 +2144,7 @@ void ProcessEntities(CppTokenVector& in_src, XEntities& ent) {
   // is wrong. Easy to detect but hard to fix completely. We try
   // several times exchanging the order.
   if (ent.code.size() > 1) {
-    int tries = 20;
+    int tries = 30;
     bool go_again = true;
     while ((--tries != 0) && go_again) {
       go_again = false;
