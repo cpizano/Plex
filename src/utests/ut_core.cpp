9575f01670e73a5bf6547f29787d8efe8a688b55
Carlos Pizano <cpu@chromium.org>
2014-10-12 16:02:38 -0700

    demand paged memory prototype ready

--- src/utests/ut_core.cpp
+++ src/utests/ut_core.cpp
@@ -1247,14 +1247,16 @@ void Test_VEHManager::Exec() {
 
 void Test_DemandPagedMemory::Exec() {
 class DemandPagedMemory {
+  size_t page_faults_;
   size_t block_size_;
   plx::Range<uint8_t> range_;
-
+  
 public:
   DemandPagedMemory(size_t max_bytes, size_t block_pages)
-      : block_size_(block_pages * 4096),
+      : page_faults_(0),
+        block_size_(block_pages * 4096),
         range_(reinterpret_cast<uint8_t*>(
-          ::VirtualAlloc(NULL, max_bytes, MEM_RESERVE, PAGE_NOACCESS)), max_bytes) {
+            ::VirtualAlloc(NULL, max_bytes, MEM_RESERVE, PAGE_NOACCESS)), max_bytes) {
     if (!range_.start())
       throw 1;
     auto veh_man = plx::Globals::get<plx::VEHManager>();
@@ -1267,8 +1269,13 @@ public:
     ::VirtualFree(range_.start(), 0, MEM_RELEASE);
   }
 
+  plx::Range<uint8_t> get() { return range_; }
+
+  size_t page_faults() const { return page_faults_; }
+
 private:
   bool page_fault(EXCEPTION_RECORD* er) {
+    ++page_faults_;
     auto addr = reinterpret_cast<uint8_t*>(er->ExceptionInformation[1]);
     auto alloc_sz = std::min(block_size_, size_t(range_.end() - addr));
     auto new_addr = ::VirtualAlloc(addr, alloc_sz, MEM_COMMIT, PAGE_READWRITE);
@@ -1276,5 +1283,17 @@ private:
   }
 };
 
+  DemandPagedMemory dmp(2*1024*1024, 2);
+  const auto page_sz = 4096UL;
+  auto r = dmp.get();
+  // page fault in the second page.
+  r[page_sz + 1] = 33;
+  // page fault in the first page.
+  r[3] = 44;
+  // no page fault.
+  r[0] = 22;
+  // no page fault.
+  r[(page_sz * 2) + 1] = 11;
+  CheckEQ(dmp.page_faults(), 2);
 }
 
