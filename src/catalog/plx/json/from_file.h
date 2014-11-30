4bf5a847987dfe17fffd903499ac9c85602cf8ad
Carlos Pizano <cpu@chromium.org>
2014-11-29 19:08:52 -0800

    tweaking plx::JsonFromFile

--- src/catalog/plx/json/from_file.h
+++ src/catalog/plx/json/from_file.h
@@ -4,14 +4,12 @@
 namespace plx {
 plx::JsonValue JsonFromFile(plx::File& cfile) {
   if (!cfile.is_valid())
-    return false;
+    throw plx::IOException(__LINE__, L"<json file>");
   auto size = cfile.size_in_bytes();
-  if (size < 3)
-    return false;
   plx::Range<uint8_t> r(0, plx::To<size_t>(size));
   auto mem = plx::HeapRange(r);
   if (cfile.read(r, 0) != size)
-    return false;
+    throw plx::IOException(__LINE__, L"<json file>");
   plx::Range<const char> json(reinterpret_cast<char*>(r.start()),
                               reinterpret_cast<char*>(r.end()));
   return plx::ParseJsonValue(json);
