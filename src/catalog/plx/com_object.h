a326fe7a7b1f0983dad0035116fc2c03743b4c7b
Carlos Pizano <cpu@chromium.org>
2015-07-11 14:15:26 -0700

    changes to support CamCenter
    - add more com support
    - add tricky MediaFoundation items to the index

--- src/catalog/plx/com_object.h
+++ src/catalog/plx/com_object.h
@@ -6,4 +6,10 @@ namespace plx {
 template <typename... T> using ComObject =
     Microsoft::WRL::RuntimeClass <
         Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>, T...>;
+
+template <typename T, typename... Args>
+Microsoft::WRL::ComPtr <T> MakeComObj(Args&&... args) {
+  return Microsoft::WRL::Make<T>(std::forward<Args>(args)...);
+}
+
 }
