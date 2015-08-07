80312089a4ed55fc63592865a0e60b387bbab314
Carlos Pizano <cpu@chromium.org>
2015-08-06 20:16:38 -0700

    add shared memory
    and move the the paged_memory to the memory/ directory

--- src/utests/stdafx.h
+++ src/utests/stdafx.h
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <nmmintrin.h>
 #include <string.h>
+#include <stdio.h>
 #include <array>
 #include <functional>
 #include <initializer_list>
@@ -2352,6 +2353,82 @@ struct SizeL : public ::SIZE {
   SizeL() : SIZE() {}
   SizeL(long width, long height) : SIZE({width, height}) {}
   bool empty() const { return (cx == 0L) && (cy == 0L); }
+};
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// plx::SharedMemory, SharedSection
+//
+class SharedMemory {
+  plx::Range<uint8_t> range_;
+  friend class SharedSection;
+
+public:
+  SharedMemory(SharedMemory&& other) : range_(other.range_) {
+    other.range_.clear();
+  }
+
+  SharedMemory(const SharedMemory&) = delete;
+  SharedMemory& operator=(const SharedMemory&) = delete;
+
+  ~SharedMemory() {
+    if (!range_.empty())
+      ::UnmapViewOfFile(range_.start());
+  }
+
+  plx::Range<uint8_t> range() { return range_; }
+
+protected:
+  SharedMemory(const plx::Range<uint8_t>& range)
+      : range_(range) {
+  }
+
+};
+
+class SharedSection {
+  HANDLE mapping_;
+
+public:
+  enum SP {
+    section_r = PAGE_READONLY,
+    section_rw = PAGE_READWRITE,
+  };
+
+  enum MP {
+    map_r = SECTION_MAP_READ,
+    map_rw = SECTION_MAP_READ | SECTION_MAP_WRITE
+  };
+
+  SharedSection(const std::wstring name, SP protect, size_t size) : mapping_(0) {
+    LARGE_INTEGER li;
+    li.QuadPart = size;
+    mapping_ = ::CreateFileMapping(INVALID_HANDLE_VALUE,
+                                   nullptr,
+                                   protect,
+                                   li.HighPart, li.LowPart,
+                                   name.c_str());
+    if (!mapping_)
+      throw 1;
+  }
+
+  ~SharedSection() {
+    ::CloseHandle(mapping_);
+  }
+
+  SharedSection(const SharedMemory&) = delete;
+  SharedSection& operator=(const SharedMemory&) = delete;
+
+  SharedMemory map(size_t start, size_t size, MP protect) {
+    LARGE_INTEGER li;
+    li.QuadPart = start;
+    auto addr = reinterpret_cast<uint8_t*>(
+        ::MapViewOfFileEx(mapping_, protect, li.HighPart, li.LowPart, size, nullptr));
+    if (!addr)
+      throw 2;
+    return SharedMemory(plx::Range<uint8_t>(addr, size));
+  }
+
 };
 
 
--- src/utests/ut_core.cpp
+++ src/utests/ut_core.cpp
@@ -1329,81 +1329,10 @@ void Test_RectLSizeL::Exec() {
   CheckEQ(r2.height() == 20, true);
 }
 
-class SharedMemory {
-  plx::Range<uint8_t> range_;
-  friend class SharedSection;
-
-public:
-  SharedMemory(SharedMemory&& other) : range_(other.range_) {
-    other.range_.clear();
-  }
-
-  SharedMemory(const SharedMemory&) = delete;
-  SharedMemory& operator=(const SharedMemory&) = delete;
-
-  ~SharedMemory() {
-    if (!range_.empty())
-      ::UnmapViewOfFile(range_.start());
-  }
-
-  plx::Range<uint8_t> range() { return range_; }
-
-protected:
-  SharedMemory(const plx::Range<uint8_t>& range)
-    : range_(range) {
-  }
-
-};
-
-class SharedSection {
-  HANDLE mapping_;
-
-public:
-  enum SP {
-    section_r = PAGE_READONLY,
-    section_rw = PAGE_READWRITE,
-  };
-
-  enum MP {
-    map_r = SECTION_MAP_READ,
-    map_rw = SECTION_MAP_READ | SECTION_MAP_WRITE
-  };
-
-  SharedSection(const std::wstring name, SP protect, size_t size) : mapping_(0) {
-    LARGE_INTEGER li;
-    li.QuadPart = size;
-    mapping_ = ::CreateFileMapping(INVALID_HANDLE_VALUE,
-                                   nullptr,
-                                   protect,
-                                   li.HighPart, li.LowPart,
-                                   name.c_str());
-    if (!mapping_)
-      throw 1;
-  }
-
-  ~SharedSection() {
-    ::CloseHandle(mapping_);
-  }
-
-  SharedSection(const SharedMemory&) = delete;
-  SharedSection& operator=(const SharedMemory&) = delete;
-
-  SharedMemory map(size_t start, size_t size, MP protect) {
-    LARGE_INTEGER li;
-    li.QuadPart = start;
-    auto addr = reinterpret_cast<uint8_t*>(
-        ::MapViewOfFileEx(mapping_, protect, li.HighPart, li.LowPart, size, nullptr));
-    if (!addr)
-      throw 2;
-    return SharedMemory(plx::Range<uint8_t>(addr, size));
-  }
-
-};
-
 void Test_SharedMemory::Exec() {
-  SharedSection section(L"lala_foo", SharedSection::section_rw, 8 * 1024);
-  auto sh_mem1 = section.map(0, 4 * 1024, SharedSection::map_rw);
-  auto sh_mem2 = section.map(0, 2 * 1024, SharedSection::map_r);
+  plx::SharedSection section(L"lala_foo", plx::SharedSection::section_rw, 8 * 1024);
+  auto sh_mem1 = section.map(0, 4 * 1024, plx::SharedSection::map_rw);
+  auto sh_mem2 = section.map(0, 2 * 1024, plx::SharedSection::map_r);
 
   sh_mem1.range()[5] = 0x66;
   sh_mem1.range()[6] = 0x55;
@@ -1428,6 +1357,5 @@ void Test_SharedMemory::Exec() {
     master will read the message table and use it to decode each message.
 07. The master can reset the head value, the clients only increment it. The master resets the head
     when the messages have been written to disk.
-
-
 */
+
