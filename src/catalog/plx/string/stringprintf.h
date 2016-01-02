1515ce319f3c9d51c820db03b81cb68e6aa7550f
Carlos Pizano-Uribe <carlos.pizano@gmail.com>
2016-01-01 23:31:44 -0800

    add a wchar version of plx::StringPrintf

--- src/catalog/plx/string/stringprintf.h
+++ src/catalog/plx/string/stringprintf.h
@@ -1,10 +1,10 @@
 //#~def plx::StringPrintf
+//#~def plx::vsnprintf
 ///////////////////////////////////////////////////////////////////////////////
 // plx::StringPrintf  (c-style printf for std strings)
 //
 namespace plx {
 
-namespace impl_v {
 int vsnprintf(char* buffer, size_t size,
               const char* format, va_list arguments) {
   int length = _vsprintf_p(buffer, size, format, arguments);
@@ -15,25 +15,38 @@ int vsnprintf(char* buffer, size_t size,
   }
   return length;
 }
+
+int vsnprintf(wchar_t* buffer, size_t size,
+              const wchar_t* format, va_list arguments) {
+  int length = _vswprintf_p(buffer, size, format, arguments);
+  if (length < 0) {
+    if (size > 0)
+      buffer[0] = 0;
+    return _vscwprintf_p(format, arguments);
+  }
+  return length;
 }
 
-std::string StringPrintf(const char* fmt, ...) {
+
+template <typename CH>
+std::basic_string<CH> StringPrintf(const CH* fmt, ...) {
   int fmt_size = 128;
-  std::unique_ptr<char> mem;
+  std::unique_ptr<CH> mem;
 
   va_list args;
   va_start(args, fmt);
 
   while (true) {
-    mem.reset(new char[fmt_size]);
-    int sz = impl_v::vsnprintf(mem.get(), fmt_size, fmt, args);
+    mem.reset(new CH[fmt_size]);
+    int sz = vsnprintf(mem.get(), fmt_size, fmt, args);
     if (sz < fmt_size)
       break;
     fmt_size = sz + 1;
   }
 
   va_end(args);
-  return std::string(mem.get());
+  return mem.get();
 }
+
 
 }
